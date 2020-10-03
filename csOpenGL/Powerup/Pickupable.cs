dad7d67487209ef33e216e4c3aa7657c58ebf522
Matthijs Hartskeerl <matthijs@hartskeerl.nl>
2020-10-03 14:15:54 +0200

    Pickupable

---
+++ csOpenGL/Powerup/Pickupable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD47.Powerup
+{
+    abstract class Pickupable
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Sprite Sprite { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
