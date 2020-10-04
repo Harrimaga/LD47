bc3d01fceed54a530cc982160f3d4e5479dea014
KingTurtleTheFrog <timvanbrederode@live.nl>
2020-10-04 12:30:10 +0200

    Added ground AA texture

--- csOpenGL/Texture.cs
+++ csOpenGL/Texture.cs
@@ -13,7 +13,7 @@ namespace LD47
     {
 
         public static List<Texture> list = new List<Texture>();
-        public const int test = 0, pixel = 1, testLevel = 2, UKBomber1gun = 3, baseProjectile = 4, GERBomber1gun = 5, MapLondonDortmund = 6, clouds1 = 7, clouds2 = 8, clouds3 = 9, GERAABoat = 10;
+        public const int test = 0, pixel = 1, testLevel = 2, UKBomber1gun = 3, baseProjectile = 4, GERBomber1gun = 5, MapLondonDortmund = 6, clouds1 = 7, clouds2 = 8, clouds3 = 9, GERAABoat = 10, GERAAGround = 11;
 
         public static void Load()
         {
@@ -28,6 +28,7 @@ namespace LD47
             list.Add(new Texture("Textures/Maps/Clouds2.png", 800, 12000, 800, Level.gameHeight));
             list.Add(new Texture("Textures/Maps/Clouds3.png", 800, 12000, 800, Level.gameHeight));
             list.Add(new Texture("Textures/Ships/Germany/GERAABoat.png", 128, 128, 64, 128));
+            list.Add(new Texture("Textures/Ships/Germany/GERAAGround.png", 64, 64, 64, 64));
         }
 
         public static Texture Get(int i)
